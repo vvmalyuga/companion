cc8fb7e9f83e275aba7816cf9d17421c559d066a
vvmalyuga <144385580+vvmalyuga@users.noreply.github.com>
2025-12-14 19:19:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-document.querySelectorAll("button").forEach(btn => {
-  btn.addEventListener("click", () => {
-    alert("Заявка на бронирование отправлена!");
-  });
-});
+function login() {
+  const user = document.getElementById('username').value;
+  const pass = document.getElementById('password').value;
+  if(user && pass){
+    alert('Вход выполнен успешно!');
+    window.location.href = "about.html";
+  } else {
+    alert('Введите логин и пароль');
+  }
+}
